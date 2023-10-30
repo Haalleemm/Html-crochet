2a8d6d90f9e5bf5abe433659bf87eec7079ddf0f
Onibudo AbdulHaleem <114316163+Haalleemm@users.noreply.github.com>
2023-10-30 13:48:02 +0100

    Add files via upload

---
+++ index.js
@@ -0,0 +1,9 @@
+const items = [
+    {name: "Roheem", Price: 100},
+    { name: "Haleem", Price: 200},
+    { name: "Rasheed", Price: 300},
+    {name: "Haleemah", Price: 400}
+]
+
+const filteredItems = items.filter(items.price)
+console.log(filteredItems);
